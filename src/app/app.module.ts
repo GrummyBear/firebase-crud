12f8ce0500e75f7cc4bc1c1e146f706ac8f9e1c2
GrummyBear <ga.smith97@hotmail.co.uk>
2017-02-20 15:46:37 +0000

    2nd tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
-
+import { AngularFireModule } from 'angularfire2';
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyADGs6S0Sty4lYF2Ua9P40wmnmx2lM9h5A",
+  authDomain: "fir-crud-804db.firebaseapp.com",
+  databaseURL: "https://fir-crud-804db.firebaseio.com",
+  storageBucket: "fir-crud-804db.appspot.com",
+  messagingSenderId: "92666157468"
+};
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
+
+// src/app/app.module.ts
+import {AF} from "../providers/af";
+import { LoginPageComponent } from './login-page/login-page.component';
+@NgModule(  declarations: [LoginPageComponent]
+{
+    ....,
+  providers: [AF]
+})
